8261cfdbb224c42668d039c44c6d7d2831f7b1c0
Marco Brandt <brandt.marco05@gmail.com>
2023-01-26 20:45:16 +0100

    Schulaufgaben

--- Marie/MBED/Timer.cpp
+++ Marie/MBED/Timer.cpp
@@ -8,44 +8,63 @@
 #include "LCD.h"
 #include <algorithm>
 
+//Deklaration der LCD
 lcd myLCD;
 
+//Taster als Interrupt deklariert
 InterruptIn pa1(PA_1);
 InterruptIn pa6(PA_6);
 InterruptIn pa10(PA_10);
+//Taster als Interrupt deklariert
 InterruptIn pb4(PB_4);
 InterruptIn pb3(PB_3);
 
+//Variablen initialisiert
 long sekunden = 0;
 long minuten = 0;
 long stunden = 0;
 bool running = false;
 
-void isr_1() { stunden++; }
+//ISR Methoden erstellt
+void isr_1() { 
+  //Stunden werden eins hochgezaehlt, bei Knopfdruck auf PA_1
+  stunden++; 
+}
 
 void isr_6() {
+  //Minuten werden eins hochgezaehlt, bei Knopfdruck auf PA_6
   minuten++;
+  //wenn minuten (60 mal auf Taster gedrueckt) auf 60 => in Verzweigung
   if (minuten == 60) {
+    //Minuten auf 0 setzen
     minuten = 0;
   }
 }
 
 void isr_10() {
+  //Sekunden werden eins hochgezaehlt, bei Knopfdruck auf PA_10
   sekunden++;
+  //wenn Sekunden (60 Mal auf Taster gedrueckt) auf 60 => in Verzweigung
   if (sekunden == 60) {
+    //Sekunden auf 0 setzen
     sekunden = 0;
   }
 }
 
-void isr_4() { running = !running; }
+void isr_4() { 
+  //Start und Stop isr
+  running = !running; 
+} 
 
 void isr_3() {
+  //ruecksetzen auf 0 isr
   stunden = 0;
-  minuten = 0;
+  minute = 0;
   sekunden = 0;
 }
 
 void enable() {
+  //alle Taster bis auf den 'Start/Stopp Knopf' wieder freigeben
   pa1.enable_irq();
   pa6.enable_irq();
   pa10.enable_irq();
@@ -53,6 +72,7 @@ void enable() {
 }
 
 void disable() {
+  //alle Taster bis auf den 'Start/Stopp Knopf' wieder sperren
   pa1.disable_irq();
   pa6.disable_irq();
   pa10.disable_irq();
@@ -61,59 +81,86 @@ void disable() {
 
 int main() {
 
-  
+  //TIM6 mit Takt versorgen
   RCC->APB1ENR |= 0b10000;
 
+  //Counter auf 0 setzen
   TIM6->CNT = 0;
+  //UIF auf 0 setzen
   TIM6->SR = 0;
 
   
+  //Prescaler setzen auf 1ms
   TIM6->PSC = 31999;
   
+  //Auto Reload Register setzen, auf 1.000 Durchlaeufen
   TIM6->ARR = 999;
 
+  //Timer starten
   TIM6->CR1 = 1;
 
   myLCD.clear();
 
+  //Taster initialisieren, auf PullDown setzen
   pa1.mode(PullDown);
   pa6.mode(PullDown);
   pa10.mode(PullDown);
   pb4.mode(PullDown);
   pb3.mode(PullDown);
 
+  //Taster PB_4 freigeben (einzeln, da in Methodenaufruf kurz darauf, nicht enthalten)
   pb4.enable_irq();
+  //Methodenaufruf
   enable();
 
+  //isr Adressen zuweisen
   pa1.rise(&isr_1);
   pa6.rise(&isr_6);
   pa10.rise(&isr_10);
   pb4.rise(&isr_4);
   pb3.rise(&isr_3);
 
+  //Endlosschleife
   while (true) {
+    //lcd Cursorposition zuweisen
     myLCD.cursorpos(0);
+    //lcd Anzeige
     myLCD.printf("%dh : %dm : %ds      ", stunden, minuten, sekunden);
 
+
+    //wenn running == true => disable Methode aufgerufen
     if (running) {
       disable();
+      //wenn UIF auf 1 => in Verzweigung
       if (TIM6->SR == 1) {
+        //UIF auf 0 setzen
         TIM6->SR = 0;
+        //Sekunden eins abziehen
         sekunden--;
       }
+      //wenn sekunden auf -1 => in Verzweigung
       if (sekunden == -1) {
+        //Sekunden auf 59 setzen
         sekunden = 59;
+        //Minuten eins abziehen
         minuten--;
       }
+      //wenn minuten auf -1 => in Verzweigung
       if (minuten == -1) {
+        //minuten auf 59 setzen
         minuten = 59;
+        //Stunden eins abziehen
         stunden--;
       }
+      //wenn stunden auf 0 und minuten auf 0 und sekunden auf 0 => in Verzweigung
       if (stunden == 0 && minuten == 0 && sekunden == 0) {
+        //running auf false gesetzt
         running = false;
+        //Methode enable aufgerufen 
         enable();
       }
     } else {
+      //Methode enable aufgerufen
       enable();
     }
   }
--- Marie/MBED/Uhr.cpp
+++ Marie/MBED/Uhr.cpp
@@ -6,16 +6,20 @@
 #include "mbed.h"
 
 #include "LCD.h"
+// Lcd Anzeige deklarieren
 lcd myLCD;
 
+//Taster als Interrupt deklariert
 InterruptIn pa1(PA_1);
 InterruptIn pa6(PA_6);
 InterruptIn pa10(PA_10);
 
+//Variablen initialisiert
 long sekunden = 0;
 long minuten = 0;
 long stunden = 0;
 
+//Interrupt Service Routinen
 void isr_1() { stunden++; }
 
 void isr_6() { minuten++; }
@@ -24,55 +28,83 @@ void isr_10() { sekunden++; }
 
 int main() {
 
-  // Timer
+  // Timer mit Takt versorgen, TIM6
   RCC->APB1ENR |= 0b10000;
 
+  //gezaehlte Durchlaeufe von ARR zurücksetzen
   TIM6->CNT = 0;
+
+  //Update Interrupt Flag (UIF) auf 0 setzen
   TIM6->SR = 0;
 
-  // Ist eine ms
+  //Ein Durchlauf -> 1ms
   TIM6->PSC = 31999;
-  // Ist dann mal tausend eine s
+
+  //Muss 999 Mal durchlaufen, um Flag auszulösen 
+  //Beachten 0 zaehlt mit -> 1.000 Durchlaefe
   TIM6->ARR = 999;
 
+  /*
+  
+  100%          Maschienenzyklen in 1ms oder 1ns
+                |                  
+                |                 Verlangsamt auf von uns gegebene Durchlaufdauer 
+                |                 |                         Zählt uns die Durchläufe von AutoReload
+  (Timer)-->-->(PreSCaler)-->-->(AutoReloadRegister)-->-->(CouNTer)
+
+  */  
+
+  //Timer starten
   TIM6->CR1 = 1;
 
   myLCD.clear();
 
+  //Taster initialisieren
   pa1.mode(PullDown);
   pa6.mode(PullDown);
   pa10.mode(PullDown);
+  //Taster isr freigeben
   pa1.enable_irq();
   pa6.enable_irq();
   pa10.enable_irq();
+  //auf steigende Flanke reagieren
   pa1.rise(&isr_1);
   pa6.rise(&isr_6);
   pa10.rise(&isr_10);
 
-  bool running = true;
-
+  //Endlosschleife
   while (true) {
+    //Cursor positionieren
     myLCD.cursorpos(0);
+    //Anzeige freihalten fuer Werte
     myLCD.printf("%d : %d : %d", stunden, minuten, sekunden);
 
-    if (running) {
-
-      if (TIM6->SR == 1) {
+    //wenn UIF auf 1 -> ein Durchlauf fertig => in Verzweigung
+    if (TIM6->SR == 1) {
+        //UIF auf 0 setzen
         TIM6->SR = 0;
+        //Sekunden eins hochzaehlen
         sekunden++;
-      }
-      if (sekunden == 60) {
-        sekunden = 0;
-        minuten++;
-      }
-      if (minuten == 60) {
-        minuten = 0;
-        stunden++;
-      }
-      if (stunden == 24) {
-        stunden = 0;
-      }
-    } else {
     }
+    //wenn sekunden auf 60 hochgezaehlt wurde => in Verzweigung
+    if (sekunden == 60) {
+      //Sekunden auf 0 setzen
+      sekunden = 0;
+      //Minuten eins hochzaehlen
+      minuten++;      
+    }
+    //wenn minuten auf 60 hochgezaehlt => in Verzweigung
+    if (minuten == 60) {
+      //Minuten auf 0 setzen
+      minuten = 0;
+      //Stunden eins hochzaehlen
+      stunden++;
+    }
+    //wenn stunden auf 24 hochgezaehlt => in Verzweigung
+    if (stunden == 24) {
+      //Stunden auf 0 setzen 
+      stunden = 0;
+    }
+    
   }
 }
