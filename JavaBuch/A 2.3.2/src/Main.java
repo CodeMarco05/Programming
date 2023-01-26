2a0a076c44a289aa958b5fd71ba6732fcc120d3a
Marco Brandt <brandt.marco05@gmail.com>
2023-01-26 21:31:09 +0100

    2.3.2

---
+++ JavaBuch/A 2.3.2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Number of bottles in total?");
+        int bottles = scanner.nextInt();
+        int captain = bottles / 2;
+        System.out.println("Bottles for the captain: " + captain);
+
+        int crewBottles = bottles - captain;
+        System.out.println("Bottles for all crew members: " + crewBottles);
+
+        System.out.println("How many crew members?");
+        int crewMembers = scanner.nextInt();
+
+        boolean even = crewBottles % crewMembers == 0 ? true: false;
+
+        System.out.println("Faire share without remainder? " + even);
+    }
+}
