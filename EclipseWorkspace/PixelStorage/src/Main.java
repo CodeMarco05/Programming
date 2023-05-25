9e985f948e785fde74317d05348b9065bbf7910f
Marco <brandt.marco05@gmail.com>
2023-05-25 08:53:15 +0200

    working Encode and Decode(UTF8)

--- EclipseWorkspace/PixelStorage/src/Main.java
+++ EclipseWorkspace/PixelStorage/src/Main.java
@@ -16,38 +16,72 @@ public class Main {
 
 	static String picturePath = System.getProperty("user.home") + "/Desktop/dotted_image.png";
 	static String txtOutPath = System.getProperty("user.home") + "/Desktop/outputTXT.txt";
+	static String txtInputPath = System.getProperty("user.home") + "/Desktop/inputTXT.txt";
 
 	public static void main(String[] args) {
 
-		ArrayList<String> binary = readTXTFileT0Bin();
-		
-		for(String s : binary) {
-			System.out.println(s);
-		}
-		
-		System.out.println(binary.size());
-		
-		//int[] numbs = findMultipliers(binary.length());
-		
+		ArrayList<String> binary = readTXTFileToBin(txtInputPath);
+		System.out.println("Finished Reading Input!");
 
-		//createImage(binary);
-		
-		
-		//String text = convertBinaryToText(binary);
-	
-		//writeTextToFile(text, txtOutPath);
+		String binaryString = String.join("", binary);
+
+		createImage(binaryString);
+		System.out.println("Finished creating Image");
+
+		ArrayList<String> binaryInterpret = interpretDotmap(1, picturePath);
+
+		String interpretBinary = String.join("", binaryInterpret);
+
+		// writeBinaryToText(interpretBinary, txtOutPath);
+
+		// writeTextToFile(binaryString, txtOutPath);
+
+		// int[] numbs = findMultipliers(binary.length());
+
+		// createImage(binary);
+
+		// String text = convertBinaryToText(binary);
+
+		// writeTextToFile(text, txtOutPath);
 
 		System.out.println("Finished");
 
 	}
-	
-	public static void writeTextToFile(String text, String filePath) {
-	    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-	        writer.write(text);
-	        System.out.println("Text written to file successfully.");
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
+
+	public static void writeBinaryToText(String binary, String filePath) {
+
+		StringBuilder textBuilder = new StringBuilder();
+
+		while (binary.length() > 0) {
+			if (binary.charAt(0) == '0') {
+				// Take the eight bits for encoding
+				String bits = binary.substring(0, 8);
+				// Slice the part off with the encoded binarys
+				binary = binary.substring(8, binary.length());
+				// Get the char for the binary
+				char c = decodeEightBit(bits);
+				textBuilder.append(c);
+
+			} else {
+				String bits = binary.substring(0, 8);
+				binary = binary.substring(8, binary.length());
+				char c = decodeSixteenBit(bits);
+
+				textBuilder.append(c);
+			}
+		}
+
+		writeDecodedStringToFile(textBuilder.toString(), filePath);
+	}
+
+	public static char decodeSixteenBit(String sixteenBitBinary) {
+		int code = Integer.parseInt(sixteenBitBinary, 2);
+
+		return (char) code;
+	}
+
+	public static char decodeEightBit(String bits) {
+		return (char) Integer.parseInt(bits, 2);
 	}
 
 	public static void createImage(String binaryCode) {
@@ -55,10 +89,12 @@ public class Main {
 
 		int[] multipliers = findMultipliers(code.length);
 
+		System.out.println(code.length);
+
 		int width = multipliers[0];
 		int height = multipliers[1];
-		
-		
+
+		System.out.println(width + " " + height);
 
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
 		Graphics2D graphics = image.createGraphics();
@@ -72,13 +108,13 @@ public class Main {
 		int index = 0;
 
 		for (int i = 0; i < height; i += dotSize) {
-	        for (int j = 0; j < width; j += dotSize) {
-	            if (code[index] == '1') {
-	                graphics.fillRect(j, i, dotSize, dotSize);
-	            }
-	            index++;
-	        }
-	    }
+			for (int j = 0; j < width; j += dotSize) {
+				if (code[index] == '1') {
+					graphics.fillRect(j, i, dotSize, dotSize);
+				}
+				index++;
+			}
+		}
 
 		graphics.dispose();
 
@@ -91,61 +127,62 @@ public class Main {
 
 	}
 
-	public static int[] findMultipliers(int evenNumber) {
-		if (evenNumber % 2 != 0) {
-			// Check if the number is not even
-			return null;
-		}
+	public static int[] findMultipliers(int number) {
 
-		int highestMultiplier1 = 1;
-		int highestMultiplier2 = evenNumber;
+		//TODO: the method wont work anymore
+		int[] multipliers = new int[2];
 
-		for (int i = 2; i <= Math.sqrt(evenNumber); i++) {
-			if (evenNumber % i == 0) {
-				int quotient = evenNumber / i;
-				if (i > highestMultiplier1) {
-					highestMultiplier1 = i;
-					highestMultiplier2 = quotient;
-				} else if (i == highestMultiplier1 && quotient > highestMultiplier2) {
-					highestMultiplier2 = quotient;
-				}
+		// Check if the number is even
+		if (number % 2 == 0) {
+			// Divide by 2 to find the highest factor
+			int highestFactor = number / 2;
+
+			// Divide by 2 until we can no longer divide evenly
+			int secondFactor = highestFactor;
+			while (secondFactor % 2 == 0) {
+				secondFactor /= 2;
 			}
+
+			multipliers[0] = highestFactor;
+			multipliers[1] = secondFactor;
+		} else {
+			// Handle case when the number is odd
+			multipliers[0] = number;
+			multipliers[1] = 1;
 		}
 
-		if (highestMultiplier1 == 1 && highestMultiplier2 == evenNumber) {
-			// No proper multipliers found
-			return null;
-		}
-
-		return new int[] { highestMultiplier1, highestMultiplier2 };
+		return multipliers;
 
 	}
 
-	public static ArrayList<String> readTXTFileT0Bin() {
-		
-
-		String filePath = "C:/Users/brand/Desktop/test.txt";
+	public static ArrayList<String> readTXTFileToBin(String path) {
 
 		ArrayList<String> result = new ArrayList<>();
 
-		try (BufferedReader reader = new BufferedReader(
-				new InputStreamReader(new FileInputStream(filePath), StandardCharsets.ISO_8859_1))) {
-			
-			
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)))) {
+
 			String line;
-			
+
 			while ((line = reader.readLine()) != null) {
-			
+
 				String lineResult = "";
 				for (char c : line.toCharArray()) {
-					//Get ascii from every character in the line and add it
+					// Get ASCII from every character in the line and add it
 					int ascii = (int) c;
-					
-					lineResult += (Integer.toBinaryString(ascii) + " ");
+
+					String binary = Integer.toBinaryString(ascii);
+
+					// Format binary to 8 bit length
+					while (binary.length() < 8) {
+						binary = "0" + binary;
+					}
+					// Remove the blank space for the image
+					// lineResult += binary + " ";
+					lineResult += binary;
 				}
-				
-				lineResult += "00" + Integer.toBinaryString(10);
-				
+
+				lineResult += "0000" + Integer.toBinaryString(10);
+
 				result.add(lineResult);
 			}
 		} catch (IOException e) {
@@ -155,96 +192,66 @@ public class Main {
 		return result;
 	}
 
-	public static String interpretDotmap(int dotSize) {
-		StringBuilder result = new StringBuilder();
+	public static ArrayList<String> interpretDotmap(int dotSize, String path) {
+
+		ArrayList<String> result = new ArrayList<>();
 
 		try {
-			BufferedImage image = ImageIO.read(new File(picturePath));
+			BufferedImage image = ImageIO.read(new File(path));
 
 			int width = image.getWidth();
 			int height = image.getHeight();
 
 			for (int y = 0; y < height; y += dotSize) {
+
+				StringBuilder lineBinary = new StringBuilder();
+
+				// Curses through every line and collects the data
 				for (int x = 0; x < width; x += dotSize) {
 					int rgb = image.getRGB(x, y);
 					if (rgb == Color.BLACK.getRGB()) {
-						result.append("1");
+						lineBinary.append("1");
 					} else {
-						result.append("0");
+						lineBinary.append("0");
 					}
 				}
+				// End of line
+				result.add(lineBinary.toString());
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return convertBinaryToString(result.toString());
+		return result;
 	}
 
-	public static void writeTXTFile(String binaryCode) {
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(txtOutPath))) {
-			int length = binaryCode.length();
+	public static void writeTXTFile(String text, String filePath) throws IOException {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+			writer.write(text);
+		}
+	}
 
-			for (int i = 0; i < length; i += 8) {
-				String byteString = binaryCode.substring(i, Math.min(i + 8, length));
-				int byteValue;
-				try {
-					byteValue = Integer.parseInt(byteString, 2);
-				} catch (NumberFormatException e) {
-					e.printStackTrace();
-					continue; // Skip invalid characters
-				}
-				writer.write(convertIntToChar(byteValue));
-			}
-
-			System.out.println("Characters written to file successfully.");
+	public static void writeDecodedStringToFile(String text, String filepath) {
+		try (BufferedWriter writer = new BufferedWriter(
+				new OutputStreamWriter(new FileOutputStream(filepath), StandardCharsets.UTF_8))) {
+			writer.write(text);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static String convertBinaryToString(String binary) {
-		StringBuilder stringBuilder = new StringBuilder();
-		int length = binary.length();
-		for (int i = 0; i < length; i += 8) {
-			String byteString = binary.substring(i, Math.min(i + 8, length));
-			int byteValue;
-			try {
-				byteValue = Integer.parseInt(byteString, 2);
-			} catch (NumberFormatException e) {
-				e.printStackTrace();
-				continue; // Skip invalid characters
-			}
-			stringBuilder.append(convertIntToChar(byteValue));
-		}
-		return stringBuilder.toString();
-	}
-
-	private static char convertIntToChar(int value) {
-		if (value < 0 || value > 255) {
-			throw new IllegalArgumentException("Invalid ASCII value: " + value);
-		}
-		return (char) value;
-	}
-	
-	/*public static String convertBinaryToText(String binaryCode) {
-	    StringBuilder result = new StringBuilder();
-
-	    StringBuilder chunk = new StringBuilder();
-	    for (int i = 0; i < binaryCode.length(); i++) {
-	        char c = binaryCode.charAt(i);
-	        chunk.append(c);
-	        if (chunk.length() == 8) {
-	            if (!chunk.toString().equals("00000000")) {
-	                int charCode = Integer.parseInt(chunk.toString(), 2);
-	                result.append((char) charCode);
-	            }
-	            chunk.setLength(0);
-	        }
-	    }
-
-	    return result.toString();
-	}*/
-
+	/*
+	 * public static String convertBinaryToText(String binaryCode) { StringBuilder
+	 * result = new StringBuilder();
+	 * 
+	 * StringBuilder chunk = new StringBuilder(); for (int i = 0; i <
+	 * binaryCode.length(); i++) { char c = binaryCode.charAt(i); chunk.append(c);
+	 * if (chunk.length() == 8) { if (!chunk.toString().equals("00000000")) { int
+	 * charCode = Integer.parseInt(chunk.toString(), 2); result.append((char)
+	 * charCode); } chunk.setLength(0); } }
+	 * 
+	 * return result.toString(); }
+	 */
 
 }
